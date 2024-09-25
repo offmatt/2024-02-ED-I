2813d85c329ad4206810ad21251441d47cb96849
Matteo Oquendo <113255567+offmatt@users.noreply.github.com>
2024-09-25 11:39:15 -0300

    Update TLinkedList.c

--- LinkedList/InsertBegin/TLinkedList.c
+++ LinkedList/InsertBegin/TLinkedList.c
@@ -2,58 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _no{
-    int info;
-    struct _no *prox;
-}TNo;
+typedef struct _no {
+  int info;
+  struct _no *prox;
 
-struct _list{
-    TNo* inicio;
+} TNo;
+
+struct _list {
+  TNo *inicio;
 };
-TNo* TNo_createNFill(int info){
-    TNo* novo = malloc(sizeof(TNo));
-    if(novo){
-        novo->info = info;
-        novo->prox = NULL;
-    }
-    return novo;
+
+TNo *TNo_createNFill(int info) {
+  TNo *novo = malloc(sizeof(TNo));
+  if (novo) {
+    novo->info = info;
+    novo->prox = NULL;
+  }
+  return novo;
 }
 
-
-TLinkedList* TLinkedList_create(){
-    TLinkedList* list = malloc(sizeof(TLinkedList));
-    if(list != NULL)
-        list->inicio = NULL;
-    return list;
+TLinkedList *TLinkedList_create() {
+  TLinkedList *list = malloc(sizeof(TLinkedList));
+  if (list != NULL)
+    list->inicio = NULL;
+  return list;
 }
-bool TLinkedList_insert_begin(TLinkedList* list, int info){
-    TNo* novo = TNo_createNFill(info);
-    if(novo == NULL) return false;
-    if(list->inicio != NULL)
-        novo->prox = list->inicio;
+
+bool TLinkedList_insert_begin(TLinkedList *list, int info) {
+  TNo *novo = TNo_createNFill(info);
+  if (novo == NULL)
+    return false;
+  if (list->inicio != NULL)
+    novo->prox = list->inicio;
+  list->inicio = novo;
+  return true;
+}
+
+bool TLinkedList_insert_end(TLinkedList *list, int info) {
+  TNo *novo = TNo_createNFill(info);
+  if (novo == NULL)
+    return false;
+  if (list->inicio == NULL)
     list->inicio = novo;
-    return true;
-}
-void TLinkedList_print(TLinkedList* list){
-    TNo* aux = list->inicio;
-    while(aux != NULL){
-        printf("%d, ", aux->info);
-        aux = aux->prox;
-    }
-    putchar('\n');
+  else {
+    TNo *aux = list->inicio;
+    while (aux->prox != NULL)
+      aux = aux->prox;
+    aux->prox = novo;
+  }
+  return true;
 }
 
-bool TLinkedList_insert_end(TLinkedList* list, int info){
-    TNo* novo = TNo_createNFill(info);
-    if(novo == NULL) return false;
-    if(list->inicio == NULL)
-        list->inicio = novo;
-    else{
-        TNo* aux = list->inicio;
-        while(aux->prox != NULL)
-            aux = aux->prox;
-        aux->prox = novo;
-    }
-    return true;
-
+void TLinkedList_print(TLinkedList *list) {
+  TNo *aux = list->inicio;
+  while (aux != NULL) {
+    printf("%d, ", aux->info);
+    aux = aux->prox;
+  }
+  putchar('\n');
 }
